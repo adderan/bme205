333468df247a8f85f68338f60f5de81134dd363b
alden <aldenderan@yahoo.com>
2014-11-29 17:39:46 -0800

    -

--- hw8/align.py
+++ hw8/align.py
@@ -1,3 +1,5 @@
+import operator
+
 def score_a2m_global(s1, s2, subst, open_gap, extend_gap, double_gap, alphabet, alphabet_lower):
 	s1_gap = False
 	s2_gap = False
@@ -37,18 +39,15 @@ def score_a2m_global(s1, s2, subst, open_gap, extend_gap, double_gap, alphabet, 
 	return score
 
 class local_aligner:
-	def __init__(self, subst, open, extend, double):
+	def __init__(self, subst, alphabet, alphabet_lower, open, extend, double):
 		self.subst = subst
 		self.open = open
 		self.extend = extend
 		self.double = double
 		
-		self.alphabet = set([])
-		self.alphabet_lower = set([])
-		for pair, score in self.subst.items():
-			self.alphabet = self.alphabet.union(pair[0].upper())
-			self.alphabet_lower = self.alphabet_lower.union(pair[0].lower())
-	
+		self.alphabet = alphabet
+		self.alphabet_lower = alphabet_lower
+			
 
 	def score_a2m(self, s1, s2):
 		alignment_columns = []
@@ -63,61 +62,110 @@ class local_aligner:
 		local_score = score_a2m_global(trimmed_s1, trimmed_s2, self.subst, self.open, self.extend, self.double, self.alphabet, self.alphabet_lower)
 		return local_score
 
-	def update_value(i, j, Ir, M, Ic):
+
+	def update_value(self, i, j, Ir, M, Ic):
 		index = str(i) + "," + str(j)
-		new_value = self.align_matrices[index]
+		old_value = (None, None, None)
+		if index in self.align_matrices:
+			old_value = self.align_matrices[index]
+		IrNew = old_value[0]
+		MNew = old_value[1]
+		IcNew = old_value[2]
 		if Ir is not None:
-			new_value[0] = Ir
+			IrNew = Ir
 		if M is not None:
-			new_value[1] = M
+			MNew = M
 		if Ic is not None:
-			new_value[2] = Ic
+			IcNew = Ic
+		new_value = IrNew, MNew, IcNew
 		self.align_matrices[index] = new_value
 	
-	def get_value(i, j):
+	def get_value(self, i, j):
 		index = str(i) + "," + str(j)
 		return self.align_matrices[index]
+	
+	def print_matrix(self):
+		for i in range(self.row_seq_length):
+			print([get_value(i, j)[0] for j in range(self.col_seq_length)])
+				
 
-	def	align(row_seq, col_seq):
+	def	align(self, row_seq, col_seq):
 		self.align_matrices = {}
+		self.row_seq = row_seq
+		self.col_seq = col_seq
+
 		nrow = len(row_seq)
 		ncol = len(col_seq)
 
 		minus_inf = -100000
 
 
-		m00 = self.subst(row_seq[0], col_seq[0])
-		update_value(0, 0, minus_inf, m00, minus_inf)
+		m00 = self.subst[row_seq[0] + col_seq[0]]
+		self.update_value(0, 0, minus_inf, m00, minus_inf)
 
 		#Fill in first row and first column using the boundary conditions
 		for i in xrange(1, nrow):
 			j = 0
-			M = self.subst(row_seq[i], col_seq[j])
+			M = self.subst[row_seq[i] + col_seq[j]]
 			Ic = minus_inf
 
-			prev = get_value(i-1, j)
-			prevIr = prev[0]
-			prevM = prev[1]
-			prevIc = prev[2]
-			Ir = max(prevM - self.open, prevIr - self.extend, prevIc - self.double)
+			upIr, upM, upIc = self.get_value(i-1, j)
+			Ir = max(upM - self.open, upIr - self.extend, upIc - self.double)
 			
-			update_value(i, j, Ir, M, Ic)
+			self.update_value(i, j, Ir, M, Ic)
 		for j in xrange(1, ncol):
 			i = 0
-			M = self.subst(row_seq[i], col_seq[j])
+			M = self.subst[row_seq[i] + col_seq[j]]
 			Ir = minus_inf
-			prev = get_value(i, j - 1)
-			prevIr = prev[0]
-			prevM = prev[1]
-			prevIc = prev[2]
+			leftIr, leftM, leftIc = self.get_value(i, j - 1)
 
-			Ic = max(prevM - self.open, prevIr - self.double, prevIc - self.extend)
+			Ic = max(leftM - self.open, leftIr - self.double, leftIc - self.extend)
 			
-			update_value(i, j, Ir, M, Ic) 
+			self.update_value(i, j, Ir, M, Ic)
 
-			
+		#Fill in rest of matrix using recurrence relations
+		for i in xrange(1, nrow):
+			for j in xrange(1, ncol):
+				upIr, upM, upIc = self.get_value(i-1, j)
+				leftIr, leftM, leftIc = self.get_value(i, j-1)
+				diagIr, diagM, diagIc = self.get_value(i-1, j-1)
+
+				Sij = self.subst[row_seq[i] + col_seq[j]]
+
+				Ir = max(upM - self.open, upIr - self.extend, upIc - self.double)
+				Ic = max(leftM - self.open, leftIr - self.double, leftIc - self.extend)
+				M = Sij + max(0, diagIc, diagIr, diagM)
+
+				self.update_value(i, j, Ir, M, Ic)
+
+		scores = [(cell, Ir, M, Ic) for cell, (Ir, M, IC) in self.align_matrices.items()]
+		scores = sorted(scores, key = operator.itemgetter(2))
+		return scores[0]
+
+	def traceback_col_seq(self):
+		res_seq = ""
+
+		cell_scores = [(cell, Ir, M, Ic) for cell, (Ir, M, Ic) in self.align_matrices.items()]
+		cell_scores = sorted(cell_scores, key = operator.itemgetter(2), reverse = True)
+		start_cell = cell_scores[0][0]
 		
+		i = len(self.row_seq) - 1
+		j = len(self.col_seq) - 1
+		start_row, start_col = start_cell.split(sep = ",")
+
+		res_seq += '-' * (len(self.row_seq) - start_row)
+		i -= len(self.row_seq) - start_row
+		assert(i == start_row)
 		
+		while j > start_col:
+			res_seq += self.col_seq[j].lower()
+			j -= 1
+		assert(j == start_col)
+
+
+
+		
+
 
 
 class global_aligner:
